efe4ddc185e0ca1f62ef827a4f0a46f98b9e3f9c
Lil-Adonis <adenugaib24@gmail.com>
2023-10-23 23:55:15 -1100

    Unic

---
+++ src/components/homes/home/index.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import { Footer, Header, Wrapper } from '../../../layout';
+import { animationCreate } from '../../../utils/utils';
+import BlogArea from './blog-area';
+import BrandArea from './brand-area';
+import CreativeArea from './creative-area';
+import HeroArea from './hero-area';
+import NewsLetter from './news-letter';
+import ProjectArea from './project-area';
+import CountryArea from '../home-3/country-area';
+import TeamArea from '../home-3/team-area';
+import ServicesArea from './services-area';
+import MissionArea from '../home-5/mission-area';
+import Testimonial from './testimonial';
+
+const Home = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      animationCreate();
+    }, 500);
+  }, [])
+  return (
+    <Wrapper>
+      <Header />
+      <HeroArea />
+      <CreativeArea/>
+      <MissionArea/>
+      <TeamArea/>
+      <ProjectArea/>
+      <CountryArea/>
+      <BlogArea/>
+      <NewsLetter/>
+      <Footer/>
+    </Wrapper>
+  );
+};
+
+export default Home;
